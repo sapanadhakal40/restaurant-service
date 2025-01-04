9451c8ca720432cf5a8dffdd7fe045d7c555c0e4
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-01-04 20:41:54 +0200

    Adds FoodItem card with food image,description, price and add to cart button

---
+++ client/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+import { assets } from "../../assets/assets";
+
+ 
+const FoodItem = ({name,price,image,description}) => {
+  return (
+    <div className="bg-white rounded-lg shadow-md overflow-hidden transform transition-transform hover:scale-105 duration-300 ease-in-out">
+          <div className="h-48">
+        <img className="w-full h-full object-cover" src={image} alt={name} />
+        </div>
+     
+
+      <div className="p-4 ">
+        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-2 mb-3">
+          <h3 className="text-xl font-bold text-gray-800">{name}</h3>
+          <img  className="w-16 sm:w-20" src={assets.rating_starts} alt="rating" />
+        </div>
+        <p className="text-gray-600 text-sm mb-4">{description}</p>
+        <div className="flex justify-between items-center">
+          <p className="text-lg font-semibold text-orange-600">
+          
+            ${price.toFixed(2)}
+          </p>
+          <button className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 transition-colors">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+      </div>
+ 
+  )
+}
+
+export default FoodItem
