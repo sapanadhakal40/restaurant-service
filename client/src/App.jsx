73e65053ec3e8879792897ad3b72199350b38c0d
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-01-04 20:53:55 +0200

    Add footer route in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,30 @@
 
 
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar/Navbar'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home/home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/footer'
 
 
 const App = () => {
   return (
+    <Router>
     <div className="w-full h-full">
     <Navbar />
+ 
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/place-order" element={<PlaceOrder />} />
+    </Routes>
       
     </div>
+
+    <Footer/>
+    </Router>
   )
 }
 
