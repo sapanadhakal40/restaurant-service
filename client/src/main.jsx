a5f54b67495341bdcc67b10f25220ff56e586497
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-01-04 20:56:16 +0200

    Add store context provider wrapper in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import StoreContextProvider from './context/StoreContext.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <StoreContextProvider>
     <App />
-  </StrictMode>,
-)
+    </StoreContextProvider>
+
+</StrictMode>,
+);
+
+
+
+
+
+
+
+// import { TranslationProvider } from './hooks/useTranslation';
+// import TranslateText from './components/Navbar/TranslateText'
+
+{/* <TranslationProvider>
+     <TranslateText />
+      <App />
+    </TranslationProvider> */}
