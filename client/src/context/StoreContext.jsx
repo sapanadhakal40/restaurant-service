a847ea9881007d96d375f297492af812a33de913
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-01-06 20:41:26 +0200

    Update store context to include add,remove and delete cart functionality

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { createContext } from "react";
+import { createContext, useState} from "react";
 import { food_list } from "../assets/assets";
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -7,10 +7,47 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = ( props ) => {
 
-    const contextValue = {
-        food_list
+    const [cartItems, setCartItems] = useState({});
+    const [isCartOpen, setIsCartOpen] = useState(false);
 
+    const addToCart = (id) => {
+        setCartItems((prev) => ({
+            ...prev,
+            [id]: (prev[id] || 0) + 1
+        }))
+        console.log("Added to cart:", id);
     }
+
+    const removeFromCart = (id) => {
+        setCartItems((prev) => {
+          if (prev[id] > 1) {
+            return { ...prev, [id]: prev[id] - 1 };
+          } else {
+            const updatedCart = { ...prev };
+            delete updatedCart[id];
+            return updatedCart;
+          }
+        });
+      };
+      const deleteFromCart = (id) => {
+        setCartItems((prev) => {
+          const updatedCart = { ...prev };
+          delete updatedCart[id];
+          return updatedCart;
+        });
+      };
+
+    const contextValue = {
+        food_list,
+        cartItems,
+        addToCart,
+        removeFromCart,
+        deleteFromCart,
+        isCartOpen,
+        setIsCartOpen
+
+    };
+
         return (
             <StoreContext.Provider value={contextValue}>
                {props.children}
