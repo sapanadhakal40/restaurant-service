f25691aaab39652bdb9263803992de77e4b87f89
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-01-06 20:49:35 +0200

    Integrated cart toggle logic in FoodDisplay.jsx

--- client/src/components/FoodDisplay/FoodDisplay.jsx
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -5,16 +5,16 @@ import { StoreContext } from "../../context/StoreContext"
 import FoodItem from "../FoodItem/FoodItem"
 
 // eslint-disable-next-line react/prop-types
-const FoodDisplay = ({ category }) => {
+const FoodDisplay = ({ category, handleAddToCart }) => {
 
-    const { food_list } = useContext(StoreContext)
+    const { food_list} = useContext(StoreContext)
  
     return (
       <div className="container mx-auto px-8 py-8">
         <h2 className="text-3xl font-semibold text-center mb-8">Top dishes near you</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 {food_list.map((item) => {
-  console.log(item);
+  
   if (category==="all" || item.category===category){
     return (
       <FoodItem
@@ -24,6 +24,7 @@ const FoodDisplay = ({ category }) => {
         description={item.description}
         price={item.price}
         image={item.image}
+       handleAddToCart={handleAddToCart}
       />
     );
   }
