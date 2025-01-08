0cf1cecfe65e72b8ab1e25e93f43230664710aea
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-01-08 19:11:48 +0200

    Update App.jsx with navbar,sidebar content,toastify

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,34 @@
 
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Orders from "./pages/Orders/Orders";
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-function App() {
-  
 
+const App = () => {
   return (
-    <>
-      <div>
-        <h1>Admin</h1>
-      </div>
-    </>
-  )
-}
 
-export default App
+    <div className="flex">
+      <ToastContainer />
+      <Sidebar />
+      <div className="flex-grow "> 
+        <Navbar />
+        <div className="p-4">
+          <Routes>
+            <Route path="/" element={<h1>Welcome to Admin Panel</h1>} />
+            <Route path="/add-items" element={<Add />} />
+            
+            <Route path="/list-items" element={<List />} />
+            <Route path="/orders" element={<Orders />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
