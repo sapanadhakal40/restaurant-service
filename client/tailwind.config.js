fc408699ec9f02986038dcd9e77bfb2a28f296a1
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-01-13 19:48:56 +0200

    Update theme for animation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,46 @@
-/** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        outfit: ['Outfit', 'sans-serif'],
-        poppins: ['Poppins', 'sans-serif'],
-      },
-      colors: {
-        active: '#FFA500',
-      },
-    },
-  },
-  plugins: [],
-}
-
+	content: [
+	  "./index.html",
+	  "./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+	  extend: {
+		fontFamily: {
+		  outfit: ['Outfit', 'sans-serif'],
+		  poppins: ['Poppins', 'sans-serif'],
+		},
+		colors: {
+		  active: '#FFA500',
+		},
+		transitionProperty: {
+			'height': 'height',
+			'spacing': 'margin, padding',
+		  },
+		  transitionDuration: {
+			'2000': '2000ms',
+		  },
+		keyframes: {
+		  fadeIn: {
+			'0%': { opacity: '0', transform: 'translateY(10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+		  },
+		  slideUp: {
+			'0%': { 
+			  opacity: '0',
+			  transform: 'translateY(20px)'
+			},
+			'100%': { 
+			  opacity: '1',
+			  transform: 'translateY(0)'
+			},
+		  }
+		},
+		animation: {
+		  fadeIn: 'fadeIn 1s ease-out forwards',
+		  slideUp: 'slideUp 1s ease-out forwards',
+		}
+	  },
+	},
+	plugins: [],
+  }
