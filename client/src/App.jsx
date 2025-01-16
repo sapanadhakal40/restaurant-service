6a5d86321aad8e2ed96e253862c6dbced083596a
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-01-16 19:35:19 +0200

    Fix build issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from './components/Navbar/Navbar'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import Cart from './pages/Cart/cart'
+import Cart from './pages/Cart/Cart'
 import CartSidebar from './pages/Cart/CartSidebar'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/footer'
